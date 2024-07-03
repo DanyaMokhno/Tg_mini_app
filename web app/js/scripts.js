f3597636c00502d6f0d52311d7ca97e1534bd086
DanyaMokhno <d.mohno@rambler.ru>
2024-07-03 14:04:54 +0300

    ~

--- web app/js/scripts.js
+++ web app/js/scripts.js
@@ -3,3 +3,4 @@ let tg = window.Telegram.WebApp;
 // Nickname assignment
 let Nickname = document.getElementById("username");
 console.log(Nickname);
+Nickname = tg.initDataUnsafe.first_name + tg.initDataUnsafe.last_name;

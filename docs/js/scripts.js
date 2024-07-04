beb41d55b051c4dbb77cdb207aa63a125834664b
DanyaMokhno <d.mohno@rambler.ru>
2024-07-05 00:35:01 +0300

    jj

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,9 +2,10 @@ let tg = window.Telegram.WebApp;
 
 // Nickname assignment
 let Nickname = document.getElementById("username");
-console.log(Nickname);
-Nickname.innerText =
-  tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
+if (Nickname) {
+  Nickname.innerText =
+    tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
+}
 
 // Back Button
 const backButton = tg.BackButton;

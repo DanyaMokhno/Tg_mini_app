861ea6203f9d7aab0de59bd78b78cdc4b9a3b777
DanyaMokhno <d.mohno@rambler.ru>
2024-07-05 00:57:28 +0300

    jj

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -7,16 +7,16 @@ if (Nickname) {
     tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
 }
 
-// Back Button
-const backButton = tg.BackButton;
-// Показывать кнопку только если есть GET параметры
-// Показывать кнопку только если есть параметры
-// и страница не главная
-if (window.location.search && window.location.pathname !== "/") {
-  backButton.show();
-} else {
-  backButton.hide();
-}
-backButton.onClick(() => {
-  history.back();
-});
+// // Back Button
+// const backButton = tg.BackButton;
+// // Показывать кнопку только если есть GET параметры
+// // Показывать кнопку только если есть параметры
+// // и страница не главная
+// if (window.location.search && window.location.pathname !== "/") {
+//   backButton.show();
+// } else {
+//   backButton.hide();
+// }
+// backButton.onClick(() => {
+//   history.back();
+// });
